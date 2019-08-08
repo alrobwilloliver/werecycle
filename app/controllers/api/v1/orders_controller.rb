854c6c30b1709a5f7f84d136d025bc7fa7441874
Alan Oliver <alrobwilloliver@gmail.com>
2019-08-08 22:55:08 +0800

    Added customer number to show jbuilder and orders controller. Updated schema

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   def create
     @order = Order.new(order_params)
     @order.save
+    render :show
   end
 
   def show
@@ -33,7 +34,7 @@ class Api::V1::OrdersController < Api::V1::BaseController
   end
 
   def order_params
-    params.require(:order).permit(:trash_collector_id, :time, :address, :collection_status, :customer_phone_number, :customer_id)
+    params.require(:order).permit(:trash_collector_id, :time, :date, :address, :collection_status, :customer_phone_number, :customer_id)
   end
 
 end
