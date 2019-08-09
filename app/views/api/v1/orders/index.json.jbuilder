aaa3eb86d4a3f0f823b9129ecc43312a7eb7eec7
Zoe Shen <shenyuhang1998917@outlook.com>
2019-08-09 15:46:20 +0800

    i rewrite the jbuilder for show and index page in order to display customer name on both customer infos and all orders

--- app/views/api/v1/orders/index.json.jbuilder
+++ app/views/api/v1/orders/index.json.jbuilder
@@ -1,5 +1,6 @@
 json.orders do
   json.array! @orders do |order|
-    json.extract! order, :id, :time, :address, :collection_status
+    json.extract! order, :id, :time, :address, :collection_status, :customer_phone_number, :date, :customer_id
+    json.customer_info order.customer.name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-=======
 ActiveRecord::Schema.define(version: 2019_08_08_081929) do
 
   # These are extensions that must be enabled in order to support this database
@@ -39,9 +37,9 @@ ActiveRecord::Schema.define(version: 2019_08_08_081929) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "customer_id"
+    t.bigint "trash_collector_id"
     t.date "date"
     t.time "time"
-    t.bigint "trash_collector_id"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["trash_collector_id"], name: "index_orders_on_trash_collector_id"
   end
